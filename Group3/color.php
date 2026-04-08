0e88d3f9114703ceba44ba0c8db5cd6e4caf191d
voideater27 <camille.persing@colostate.edu>
2026-04-08 09:02:05 -0600

    Changed starter.php to color.php

--- Group3/color.php
+++ Group3/color.php
@@ -16,72 +16,39 @@
     </ul>
 </nav>
 
-    <form method="POST" action="starter.php"> 
-
+    <form method="POST" action="color.php"> 
         <label><p>How many Rows and Columns do you want?:&nbsp;</p></label> 
-
         <input type="text" name="rowsAndColumns"></input><br> 
-
         <label><p>How many colors do you want?:&nbsp;</p></label> 
-
         <input type="text" name="colors"></input><br> 
-
         <label><p>Generate Table -></p></label> 
-
         <input type="submit" value="Submit"></input> 
-
     </form> 
 
- 
-
     <?php 
-
     $grid = "Display grid.<br>"; 
-
     if(isset($_POST["rowsAndColumns"])){ 
-
         $numberOfRowsAndColumns = (int)$_POST["rowsAndColumns"];  
-
         if($numberOfRowsAndColumns >= 1 && $numberOfRowsAndColumns <= 26){ 
-
             echo($grid); 
-
         } 
-
         else{ 
-
             echo("Please enter a valid range of rows and columns: a number between 1-26.<br>"); 
-
         } 
-
     } 
-
     ?> 
 
- 
-
     <?php 
-
     $colorTable = "Display color table.<br>"; 
-
     if(isset($_POST["colors"])){ 
-
         $numberOfColors = (int)$_POST["colors"];  
-
         if($numberOfColors >= 1 && $numberOfColors <= 10){ 
-
             echo($colorTable); 
-
         } 
-
         else{ 
-
             echo("Please enter a valid range of colors: a number between 1-10.<br>"); 
-
         } 
-
     } 
-
     ?> 
 
 <footer>
