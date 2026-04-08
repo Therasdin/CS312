ffbb01d6df0c6c5b93c8b846683d6ead9eade7a3
voideater27 <camille.persing@colostate.edu>
2026-04-08 01:32:12 -0600

    Added image to about php

--- Group3/about.php
+++ Group3/about.php
@@ -40,7 +40,7 @@
     </div>
 
     <div class="team-card">
-        <img src="avatar3.png" alt="Person3">
+        <img src="owl_bunnydogma_2024.png" alt="Valentine's Image">
         <h3>Valentine Persing</h3>
         <p>Howdy! I’m an art history major and computer science minor. I’m currently in my senior year and ready to graduate come fall. I’m pursuing a career in archives and special collections, while currently employed at the CSU library. I love all sorts of arts and crafts and love to keep busy. I’m hoping to make my own costumes and outfits when I get the chance.  </p>
     </div>
